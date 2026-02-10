d3726b013db73d4c29151988deed6f977b267a03
Laurens <laurens@lx-workstation.crowsnest.homelab>
2026-02-10 17:21:04 +0100

    Fucked up the ula

--- config/default.nix
+++ config/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }: let
 	trivial = inputs.nixpkgs.lib.trivial;
 in {
-	flake.networking = let
+	flake.homelab.networking = let
 
 			root = "fd42:e2cd:499f";
 
@@ -14,7 +14,7 @@ in {
 			};
 		in {
 			domain = "crowsnest.homelab";
-			ulaPrefix = "fd42:e2cd:499f";
+			ulaPrefix = "fd42:e2cd:449f";
 
 			networks = {
 				backbone = mkNetwork "b";
